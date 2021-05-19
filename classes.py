e0f05cfdb08ef55f91057fcb216408beb2c55742
Mateus <mat.rggo@gmail.com>
2021-05-19 19:30:19 -0300

    Classe do astronauta adicionado

--- classes.py
+++ classes.py
@@ -34,3 +34,31 @@ class Roof(pygame.sprite.Sprite):
         self.rect.x += self.speedx
         if self.rect.right < 0:
             self.rect.x = WIDTH
+
+class Astronaut(pygame.sprite.Sprite):
+    def __init__(self, img):
+        # Construtor da classe mãe (Sprite).
+        pygame.sprite.Sprite.__init__(self)
+
+        self.image = img
+        self.rect = self.image.get_rect()
+        self.rect.centerx = 50
+        self.rect.bottom = 300
+        self.speedy = 0
+    
+    def jump(self):
+        self.speedy = -1
+    def fall(self):
+        self.speedy = 1
+
+    def update(self):
+        # Atualização da posição do astronauta
+        self.rect.y += self.speedy
+
+        # Mantem dentro da tela
+        if self.rect.top <= 50:
+            self.rect.top = 50
+            self.speedy = 0
+        if self.rect.bottom >= 270:
+            self.rect.bottom = 270
+            self.speedy = 0
