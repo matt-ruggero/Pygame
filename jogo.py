29e712d6d28694487fe017a06d0456b98a605c01
GuiKlin <guilhermegkp@al.insper.edu.br>
2021-05-25 18:00:38 -0300

    tela de inicio

--- jogo.py
+++ jogo.py
@@ -1,111 +1,156 @@
-import pygame
+import pygame, sys
 from classes import *
 from assets import *
-
+mainClock = pygame.time.Clock()
+from pygame.locals import *
 pygame.init()
-pygame.mixer.init()
 
 window = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption('Zero Gravity Run')
+font = pygame.font.SysFont(None, 20)
 
-background_img = pygame.image.load('imagens/background.jpeg').convert()
-background = pygame.transform.scale(background_img, (WIDTH, HEIGHT))
-ground_img = pygame.image.load('imagens/pedra.png').convert_alpha()
-ground = pygame.transform.scale(ground_img, (GROUND_WIDTH, GROUND_HEIGHT))
-astronaut_img = pygame.image.load('imagens/astronauta_novo.png').convert_alpha()
-astronaut = pygame.transform.scale(astronaut_img, (ASTRO_WIDTH, ASTRO_HEIGHT))
-tanque_o2_img = pygame.image.load('imagens/o2.png')
-tanque_o2 = pygame.transform.scale(tanque_o2_img, (TANQUE_WIDTH, TANQUE_HEIGHT))
-meteoro_img = pygame.image.load('imagens/pedra_azul.png').convert_alpha()
-meteoro = pygame.transform.scale(meteoro_img, (METEORO_WIDTH, METEORO_HEIGHT))
-monstro_img = pygame.image.load('imagens/monstro.png')
-monstro = pygame.transform.scale(monstro_img, (MONSTRO_WIDTH, MONSTRO_HEIGHT))
+def draw_text(text, font, color, surface, x, y):
+    textobj = font.render(text, 1, color)
+    textrect = textobj.get_rect()
+    textrect.topleft = (x, y)
+    surface.blit(textobj, textrect)
 
-# Carrega os sons do jogo
-pygame.mixer.music.load('audios/space.mp3')
-pygame.mixer.music.set_volume(0.4)
-pygame.mixer.music.play(-1)
-meteoro_sound = pygame.mixer.Sound('audios/impact.mp3')
-oxygen_sound = pygame.mixer.Sound('audios/oxygen1.mp3')
+click = False
+ 
+def main_menu():
+    while True:
+        mx, my = pygame.mouse.get_pos()
+ 
+        button_1 = pygame.Rect(240, 200, 100, 15)
+        if button_1.collidepoint((mx, my)):
+            if click:
+                game()
+        menu_img = pygame.image.load('imagens/menu.jpeg').convert()
+        window.blit(menu_img, [0, 0])
+        menu = pygame.transform.scale(menu_img, (WIDTH, HEIGHT))
+        pygame.draw.rect(window, (150, 69, 255,), button_1)
+        draw_text('click to start', font, (255, 255, 255), window, 250, 200) 
+        click = False
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == MOUSEBUTTONDOWN:
+                if event.button == 1:
+                    click = True
+ 
+        pygame.display.update()
+        mainClock.tick(60)
+ 
+def game():
+    running = True
+    while running:
 
-score = 0
-font = pygame.font.SysFont('Verdana', 20)
+        window = pygame.display.set_mode((WIDTH, HEIGHT))
+        pygame.display.set_caption('Zero Gravity Run')
 
-game = True
+        background_img = pygame.image.load('imagens/background.jpeg').convert()
+        background = pygame.transform.scale(background_img, (WIDTH, HEIGHT))
+        ground_img = pygame.image.load('imagens/pedra.png').convert_alpha()
+        ground = pygame.transform.scale(ground_img, (GROUND_WIDTH, GROUND_HEIGHT))
+        astronaut_img = pygame.image.load('imagens/astronauta_novo.png').convert_alpha()
+        astronaut = pygame.transform.scale(astronaut_img, (ASTRO_WIDTH, ASTRO_HEIGHT))
+        tanque_o2_img = pygame.image.load('imagens/o2.png')
+        tanque_o2 = pygame.transform.scale(tanque_o2_img, (TANQUE_WIDTH, TANQUE_HEIGHT))
+        meteoro_img = pygame.image.load('imagens/pedra_azul.png').convert_alpha()
+        meteoro = pygame.transform.scale(meteoro_img, (METEORO_WIDTH, METEORO_HEIGHT))
+        monstro_img = pygame.image.load('imagens/monstro.png')
+        monstro = pygame.transform.scale(monstro_img, (MONSTRO_WIDTH, MONSTRO_HEIGHT))
 
-clock = pygame.time.Clock()
-FPS = 100
+        # Carrega os sons do jogo
+        pygame.mixer.music.load('audios/space.mp3')
+        pygame.mixer.music.set_volume(0.4)
+        pygame.mixer.music.play(-1)
+        meteoro_sound = pygame.mixer.Sound('audios/impact.mp3')
+        oxygen_sound = pygame.mixer.Sound('audios/oxygen1.mp3')
 
-tanques = pygame.sprite.Group()
-all_meteoros = pygame.sprite.Group()
-all_grounds = pygame.sprite.Group()
-all_roofs = pygame.sprite.Group()
+        score = 0
+        font = pygame.font.SysFont('Verdana', 20)
 
-for i in range(0, 650, 50):
-    pedra = Ground(ground)
-    pedra.rect.x += i
-    all_grounds.add(pedra)
+        game = True
 
-for i in range(0, 650, 50):
-    pedra = Roof(ground)
-    pedra.rect.x += i
-    all_roofs.add(pedra)
+        clock = pygame.time.Clock()
+        FPS = 100
 
-for i in range(4):
-    meteor = Meteoro(meteoro)
-    all_meteoros.add(meteor)
+        tanques = pygame.sprite.Group()
+        all_meteoros = pygame.sprite.Group()
+        all_grounds = pygame.sprite.Group()
+        all_roofs = pygame.sprite.Group()
 
-astronauta = Astronaut(astronaut)
-tanque = Tanque(tanque_o2)
-tanques.add(tanque)
+        for i in range(0, 650, 50):
+            pedra = Ground(ground)
+            pedra.rect.x += i
+            all_grounds.add(pedra)
 
-while game:
-    clock.tick(FPS)
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            game = False
-        elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_UP:
-                astronauta.jump()
-            if event.key == pygame.K_DOWN:
-                astronauta.fall()
+        for i in range(0, 650, 50):
+            pedra = Roof(ground)
+            pedra.rect.x += i
+            all_roofs.add(pedra)
 
-    if astronauta.rect.centerx == 90:
-        game = False
-    
-    texto = font.render('Pontuação: {0}'.format(score), True, (255, 255, 255))
+        for i in range(4):
+            meteor = Meteoro(meteoro)
+            all_meteoros.add(meteor)
 
-    all_grounds.update()
-    all_roofs.update()
-    astronauta.update()
-    tanques.update()
-    all_meteoros.update()
+        astronauta = Astronaut(astronaut)
+        tanque = Tanque(tanque_o2)
+        tanques.add(tanque)
 
-    hits = pygame.sprite.spritecollide(astronauta, tanques, True, pygame.sprite.collide_mask)
+        while game:
+            clock.tick(FPS)
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    game = False
+                elif event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_UP:
+                        astronauta.jump()
+                    if event.key == pygame.K_DOWN:
+                        astronauta.fall()
 
-    for tanque in hits:
-        t = Tanque(tanque_o2)
-        tanques.add(t)
-        score += 10
-        oxygen_sound.play()
+            if astronauta.rect.centerx == 90:
+                game = False
+            
+            texto = font.render('Pontuação: {0}'.format(score), True, (255, 255, 255))
 
-    hits = pygame.sprite.spritecollide(astronauta, all_meteoros, True, pygame.sprite.collide_mask)
+            all_grounds.update()
+            all_roofs.update()
+            astronauta.update()
+            tanques.update()
+            all_meteoros.update()
 
-    for meteor in hits:
-        astronauta.rect.x -= 20
-        m = Meteoro(meteoro)
-        all_meteoros.add(m)
-        meteoro_sound.play()
+            hits = pygame.sprite.spritecollide(astronauta, tanques, True, pygame.sprite.collide_mask)
 
-    window.fill((0, 0, 0))
-    window.blit(background, (0, 0))
-    window.blit(astronauta.image, astronauta.rect)
-    tanques.draw(window)
-    all_meteoros.draw(window)
-    all_grounds.draw(window)
-    all_roofs.draw(window)
-    window.blit(monstro, (-20,0))
-    window.blit(texto, (10,10))
-    pygame.display.update()
+            for tanque in hits:
+                t = Tanque(tanque_o2)
+                tanques.add(t)
+                score += 10
+                oxygen_sound.play()
 
-pygame.quit()
+            hits = pygame.sprite.spritecollide(astronauta, all_meteoros, True, pygame.sprite.collide_mask)
+
+            for meteor in hits:
+                astronauta.rect.x -= 20
+                m = Meteoro(meteoro)
+                all_meteoros.add(m)
+                meteoro_sound.play()
+
+            window.fill((0, 0, 0))
+            window.blit(background, (0, 0))
+            window.blit(astronauta.image, astronauta.rect)
+            tanques.draw(window)
+            all_meteoros.draw(window)
+            all_grounds.draw(window)
+            all_roofs.draw(window)
+            window.blit(monstro, (-20,0))
+            window.blit(texto, (10,10))
+            pygame.display.update()
+
+        pygame.quit()
+        
+        pygame.display.update()
+        mainClock.tick(60)
+main_menu()
